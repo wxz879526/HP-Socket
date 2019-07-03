9a4fb106850d81e0241573ccd56c52b8435491df
My Name <my-name@chromium.org>
2019-07-03 23:24:08 +0800

    add protobuf

--- Windows/Demo/pb/create.bat
+++ Windows/Demo/pb/create.bat
@@ -2,4 +2,9 @@ set CURRENT_DIR=%cd%
 set DST_DIR=%CURRENT_DIR%\gen
 
 mkdir %DST_DIR%\cpp
+<<<<<<< HEAD
 protoc -I=%CURRENT_DIR% --cpp_out=%DST_DIR%\cpp\ %CURRENT_DIR%\AddressBook.proto ^
+=======
+protoc -I=%CURRENT_DIR% --cpp_out=%DST_DIR%\cpp\ %CURRENT_DIR%\AddressBook.proto ^
+	%CURRENT_DIR%\AddressBook2.proto
+>>>>>>> 61f7fda8... add protobuf
